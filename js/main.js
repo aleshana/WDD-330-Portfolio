07ab9f2aa0239e518619016019772e272b9d3aee
aleshana <77070854+aleshana@users.noreply.github.com>
2021-09-18 19:37:43 -0600

    Week 1 exersices
    Finishing up JS code and homepages for class.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Building an array manual of all the weeks, including a label and url.
+//Create a for each item in the array, insert it into the HTML document as a link.
+
+const weeksArray = [
+    {label: "Week 1 Work", url: "week01/index.html"},
+    {label: "Week 2 Work", url: "week02/index.html"},
+    {label: "Week 3 Work", url: "week03/index.html"},
+    {label: "Week 4 Work", url: "week04/index.html"},
+    {label: "Week 5 Work", url: "week05/index.html"},
+    {label: "Week 6 Work", url: "week06/index.html"},
+    {label: "Week 7 Work", url: "week07/index.html"}
+]
+
+const weeks = document.querySelector('ol.homepageWeeks');
+
+weeksArray.forEach(weekItem => {
+    let listItem = document.createElement('li');  
+    listItem.innerHTML = `<a href = ${weekItem.url}>${weekItem.label}</a>`;
+    console.log(listItem);
+    weeks.append(listItem);
+    
+});
